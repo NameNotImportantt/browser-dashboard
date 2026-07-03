7940f842ce33c048eba479f79a87a3a6f38cbd57
NameNotImportant <email>
2026-07-03 12:27:08 +0300

    refactor(hooks): remove dashboard re-export

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,5 +3,3 @@ export {useClock} from './useClock';
 export {useGlobalHotkeys} from './useGlobalHotkeys';
 
 export type {GlobalHotkeyDefinition} from './useGlobalHotkeys';
-
-export * from '@/dashboard';
